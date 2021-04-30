bb29bf63040bcadca8300b19a41722973dad53b9
Colin McDonnell <cmcdonnell@americanfirstfinance.com>
2021-04-30 15:41:07 -0500

    added theme toggle

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <div class="pointer-cursor" @click="toggleTheme">
+      <v-icon class="mr-2">{{
+        !this.$nuxt.$vuetify.theme.isDark
+          ? 'mdi-weather-night'
+          : 'mdi-weather-sunny'
+      }}</v-icon
+      >Theme
+    </div>
     <h1 class="text-center">ADDRESS LOOKUP</h1>
     <geocoder />
     <reverse-geocoder />
@@ -12,6 +20,16 @@ export default {
   components: {
     geocoder: () => import('../components/geocoder'),
     reverseGeocoder: () => import('../components/reverseGeocoder')
+  },
+  mounted() {
+    this.$nuxt.$vuetify.theme.isDark = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches
+  },
+  methods: {
+    toggleTheme() {
+      this.$nuxt.$vuetify.theme.isDark = !this.$nuxt.$vuetify.theme.isDark
+    }
   }
 }
 </script>
